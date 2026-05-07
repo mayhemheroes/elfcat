fb76bc69b6d5b7dd7ba0ed17c68d6452009516ae
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-05-07 16:15:15 +0000

    Fix Docker build and update Mayhem workflow

--- fuzz/fuzz_targets/elfcat-fuzz.rs
+++ fuzz/fuzz_targets/elfcat-fuzz.rs
@@ -14,34 +14,6 @@ use std::fs::File;
 use std::io::Write;
 use elf::parser::ParsedElf;
 
-
-//pub trait MaybeError<T> {
-//    fn or_exit(self, message: &str) -> T;
-//}
-//
-//impl<T, E> MaybeError<T> for Result<T, E>
-//where
-//    E: std::fmt::Display + std::fmt::Debug,
-//{
-//    fn or_exit(self, message: &str) -> T {
-//        if let Err(e) = self {
-//            eprintln!("Failed to {}: {}", message, e);
-//        }
-//
-//        self.unwrap()
-//    }
-//}
-//
-//impl<T> MaybeError<T> for Option<T> {
-//    fn or_exit(self, message: &str) -> T {
-//        if self.is_none() {
-//            eprintln!("Failed to {}", message);
-//        }
-//
-//        self.unwrap()
-//    }
-//}
-
 fuzz_target!(|data: &[u8]| {
     let temp_directory = env::temp_dir();
     let filename = temp_directory.join("file").into_os_string().into_string().unwrap();
@@ -52,16 +24,14 @@ fuzz_target!(|data: &[u8]| {
     let contents = std::fs::read(&filename);
     let contents = match contents {
         Ok(c) => c,
-        Err(error) => {
-            format!("Couldn't read file \"{}\"", filename);
+        Err(_) => {
             return ();
         },
     };
     let elf = ParsedElf::from_bytes(&filename, &contents);
     let elf = match elf {
         Ok(e) => e,
-        Err(error) => {
-            format!("Couldn't parse file \"{}\"", filename);
+        Err(_) => {
             return ();
         },
     };
@@ -69,18 +39,10 @@ fuzz_target!(|data: &[u8]| {
     let report_filename = match report_filename {
         Some(c) => c,
         None => {
-            format!("Couldn't construct file \"{}\"", filename);
             return ();
         },
     };
     let report = report_gen::generate_report(&elf);
-    let report = match report {
-        Some(c) => c,
-        None => {
-            format!("Couldn't generate report");
-            return ();
-        },
-    };
 
     std::fs::write(report_filename, report);
 });
